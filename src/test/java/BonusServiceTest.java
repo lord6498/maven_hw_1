a97b660465c1feb39447ff24face4fd33325d9f7
Nick <>
2021-01-26 14:53:55 +0300

    Added 4 new test scenarios for bonus calculator

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,71 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void shouldCalculateForRegistredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected,actual);
+
+
+    }
+
+    @Test
+    void shouldCalculateForRegistredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000_60;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected,actual);
+
+
+    }
+    @Test
+    void shouldCalculateForNotregistredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 10;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected,actual);
+
+
+    }
+
+    @Test
+    void shouldCalculateForNotregistredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000_60;
+        boolean registered = false;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected,actual);
+
+
+    }
+
+
+}
